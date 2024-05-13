3654a3d967ad102a0d1a92bba5211e9baf863ed7
giseletrindade <166648209+giseletrindade@users.noreply.github.com>
2024-05-13 17:51:36 -0300

    Create contaCorrente.kt

---
+++ contaCorrente.kt
@@ -0,0 +1,11 @@
+2)
+class ContaCorrente(var numerodaConta: int, var saldo double){
+     fun depositar(valor: Double) {
+        saldo += valor
+    }
+    
+    fun sacar(valor: Double) {
+        saldo -= valor
+        
+    }
+}
